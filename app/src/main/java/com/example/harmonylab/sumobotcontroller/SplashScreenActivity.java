a29af73aa65a9406232effcf211d91cc7ca52ef9
Harmony Lab <lead@harmonylab.com>
2018-08-25 18:50:22 -0700

    First version of working Android app for connecting to Sumobot using HC06 Bluetooth module.

---
+++ app/src/main/java/com/example/harmonylab/sumobotcontroller/SplashScreenActivity.java
@@ -0,0 +1,43 @@
+package com.example.harmonylab.sumobotcontroller;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private ImageView imImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        //LogoLauncher logoLauncher = new LogoLauncher();
+        //logoLauncher.start();
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+/*
+    private class LogoLauncher extends Thread{
+        public void run(){
+            try{
+                sleep(2000);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+
+            SplashScreenActivity.this.finish();
+        }
+    }
+*/
+}
